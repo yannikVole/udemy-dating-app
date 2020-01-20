d22afcc344f2649f97f5638ea1cfa6813e22ea0b
Yannik <yannik@puxn.de>
2020-01-20 22:58:15 +0100

    user registration

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: "app-value",
-  templateUrl: "./value.component.html",
-  styleUrls: ["./value.component.css"]
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
-export class ValueComponent implements OnInit {
+export class HomeComponent implements OnInit {
+  registerMode = false;
   values: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getValues();
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
   }
 
   getValues() {
@@ -24,5 +29,8 @@ export class ValueComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+  cancleRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
   }
 }
