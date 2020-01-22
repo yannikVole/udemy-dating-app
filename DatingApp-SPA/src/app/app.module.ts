c06f0a287acc2acb98c4199dacfa1818b2d8e32f
YanNik <y.vole@werbeagentur-gottschalg.de>
2020-01-22 18:34:06 +0100

    member list cards

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -13,10 +13,11 @@ import { AuthService } from "./_services/auth.service";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
-import { MemberListComponent } from "./member-list/member-list.component";
+import { MemberListComponent } from "./members/member-list/member-list.component";
 import { ListsComponent } from "./lists/lists.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { appRoutes } from "./routes";
+import { MemberCardComponent } from "./members/member-card/member-card.component";
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { appRoutes } from "./routes";
     RegisterComponent,
     MemberListComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
